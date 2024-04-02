e3b9f6f0b1fb8e33cb9dadb3b43908a0af4b5c42
djha09 <143820847+djha09@users.noreply.github.com>
2024-04-02 23:28:50 +0530

    Add files via upload

---
+++ tc.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/tickets.css?v=1">
+    <style>
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+</head>
+<body>
+    <div class="container">
+    <div class="logo1">
+            <img src="/img/logo (2).png" alt="">
+             <h1 class="logo">GLOBEXPLORE</h1>
+        </div>
+        <div class="bill">
+       
+             <div class="image">
+                <img src="./img/Australia.jpg" style="width=250px"alt="">
+             </div>
+             <div class="details">
+                <h1>Id:Bk-56</h1> 
+                <h1>Location:Dubai</h1>
+                <h1>Name:Bala</h1>
+                <!-- <h1></h1> -->
+             </div>
+             <div class="details">
+                <h2>Guest:5</h2> 
+                <h2>Date :2023-2-5 to 2023-5-5</h2>
+                <h2>Total:&#x20B9;5000</h2> 
+                
+             </div>
+             </div>
+             <p class="admit-one">
+			<span>Happy Journey</span>
+			<span>Happy Journey</span>
+			<span>Happy Journey</span>
+		</p>
+             <!-- <div class="name"> <h1>Id:56</h1></div>
+            <div class="guest"> <h1>Id:56</h1></div> -->
+                <!-- <div > -->
+                   
+<!-- <table id="customers">
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+ 
+</table> -->
+
+
+
+
+
+    </div>
+</body>
+</html>
